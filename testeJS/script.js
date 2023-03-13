b4bc1a58b2726c719edc9d21af6f5386b1380869
Cristian Veggian Matias <48965999+CristianVeggian@users.noreply.github.com>
2023-03-13 11:22:39 -0300

    Add função de Verificação de envio de informações

--- testeJS/script.js
+++ testeJS/script.js
@@ -1,10 +1,29 @@
-function addCard() {
-  var nome = document.getElementById("nome-card").value;
-  var divNova = document.createElement("div");
-  divNova.classList.add('card');
-  var text = document.createTextNode(nome);
-  divNova.appendChild(text);
+let validated = false;
 
-  var divAtual = document.getElementById("card-viewer");
-  divAtual.appendChild(divNova);
+function addCard() {
+	if(validated){
+		document.getElementById("warning").innerHTML = "";
+		var nome = document.getElementById("nome-card").value;
+		var divNova = document.createElement("div");
+		divNova.classList.add('card');
+		var text = document.createTextNode(nome);
+		divNova.appendChild(text);
+
+		var divAtual = document.getElementById("card-viewer");
+		divAtual.appendChild(divNova);
+	} else {
+		document.getElementById("warning").innerHTML  = "Insira um nome com mais de 6 letras";
+	}
+}
+
+function valida() {
+	
+	if (document.getElementById("nome-card").value.length < 6){
+		document.getElementById("nome-card").style.backgroundColor = 'red';
+		validated = false;
+	} else {	
+		document.getElementById("nome-card").style.backgroundColor = 'white';
+		validated = true;
+	}
+
 }
